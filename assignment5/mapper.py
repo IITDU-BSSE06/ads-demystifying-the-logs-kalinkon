ec642c36f1ca9e8c84b2ea57c49ce0d9c2506c2b
kalinkon <k.a.linkn@gmail.com>
2017-11-08 03:34:16 +0600

    Update mapper.py

---
+++ assignment5/mapper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+
+import sys
+
+for line in sys.stdin:
+    file = line.strip().split(" ")
+
+
+    if len(file)> 1:
+        
+        filePath = file[6]
+
+        print str(filePath)
