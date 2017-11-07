82a89f44873eda95af925e3a3374ecc777fe21c9
kalinkon <k.a.linkn@gmail.com>
2017-11-08 03:54:35 +0600

    Update reducer.py

---
+++ assignment3/reducer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import operator
+import sys
+
+dictionaryLibrary = dict()
+
+
+for line in sys.stdin:
+    filePath = line.strip()
+
+    if not filePath in dictionaryLibrary:
+      dictionaryLibrary[filePath] = 1
+
+    else: 
+      dictionaryLibrary[filePath] +=1
+
+result= max(dictionaryLibrary.iteritems(), key=operator.itemgetter(1))[0]
+print result
+
